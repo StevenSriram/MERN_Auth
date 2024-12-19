751be9888117d99a0561c7d862b964b5c02a4401
StevenSriram <sriram242004@gmail.com>
2024-12-19 14:41:27 +0530

    Starter Pack

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res) => {
+  res.send("signup");
+};
+
+export const logIn = async (req, res) => {
+  res.send("login");
+};
+
+export const logOut = async (req, res) => {
+  res.send("logout");
+};
