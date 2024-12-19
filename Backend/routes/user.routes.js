7b8950e225543fdbade7c127f51dc27f209c2cfa
StevenSriram <sriram242004@gmail.com>
2024-12-19 16:45:17 +0530

    feat : Login & Logout

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -5,16 +5,18 @@ import {
   logIn,
   logOut,
   verifyEmail,
+  forgotPassword,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
-
 router.post("/verify-email", verifyEmail);
 
 router.post("/login", logIn);
 
 router.post("/logout", logOut);
+
+router.post("forgot-password", forgotPassword);
 
 export default router;
