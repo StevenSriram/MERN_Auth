a71e79da2b6d19bd506f5ef26dde6dd962750655
StevenSriram <sriram242004@gmail.com>
2024-12-19 21:40:25 +0530

    feat : Reset Password

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -6,17 +6,23 @@ import {
   logOut,
   verifyEmail,
   forgotPassword,
+  resetPassword,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+// ? Signup and Verification
 router.post("/signup", signUp);
 router.post("/verify-email", verifyEmail);
 
+// ? Login and Logout
 router.post("/login", logIn);
-
 router.post("/logout", logOut);
 
+// ? Forgot User Password
 router.post("/forgot-password", forgotPassword);
+
+// ? Reset User Password with Token
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
