f7d9b63f2742be6fe0502353d09846a69ed283a1
StevenSriram <sriram242004@gmail.com>
2024-12-23 19:09:20 +0530

    fix : Improved Security

--- Backend/app.js
+++ Backend/app.js
@@ -6,10 +6,38 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/user.routes.js";
 import connectDB from "./db/configDB.js";
 
+import { rateLimit } from "express-rate-limit";
+import { rateLimitHandler } from "./utils/rateLimitHandler.js";
+
+import helmet from "helmet";
+
+import morgan from "morgan";
+
 const app = express();
 
 app.use(express.json()); // * Middleware to parse json Payload
 app.use(cookieParser()); // * Allow to parse Cookies
+
+// ! HTTP logger - debugging Purpose
+app.use(morgan("dev"));
+
+// ! setting various HTTP headers - hide X-Powered-By
+app.use(helmet());
+
+// ! use Rate Limiter to all Routes
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // Limit each IP to 5 requests per window
+  message: "Too many requests, please try again after a minute.",
+  handler: rateLimitHandler,
+  headers: true,
+});
+app.use(limiter);
+
+// * Testing Route
+app.get("/", (_, res) => {
+  res.status(200).send("Server Running...");
+});
 
 app.use("/api/auth", authRoutes);
 
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MERN_Auth
+
+## Security Features
+
+### Rate-Limiter
+
+[Rate-limiter](https://www.npmjs.com/package/express-rate-limit) controls the number of requests a user can make to a server within a specific time frame. It helps prevent abuse and ensures fair usage of resources.
+
+### Helmet
+
+[Helmet](https://www.npmjs.com/package/helmet) is a Node.js middleware that helps secure your application by setting various HTTP headers to protect against common web vulnerabilities.
+
+- **X-Powered-By** is an HTTP header that reveals the technology stack used by a web server (e.g., "Express" for Node.js). Helmet can disable this header to enhance security.
+
+### Morgan
+
+[Morgan](https://www.npmjs.com/package/morgan) is a Node.js middleware that logs HTTP request details to the console for monitoring and debugging purposes.
