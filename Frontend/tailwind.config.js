3f10474d226c3a88fe4cefd3a080f759c73cb8df
StevenSriram <sriram242004@gmail.com>
2024-12-24 15:33:07 +0530

    feat : Login UI

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: ["Lora", "sans-serif"], // Roboto font
+        montserrat: ["Montserrat", "sans-serif"], // Montserrat font
+      },
+    },
   },
   plugins: [],
 };
